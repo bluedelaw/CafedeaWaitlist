c4552a4e7bae0730cf861cc287da6c22e87eaebb
simonlotzkar <simonlotzkar@gmail.com>
2025-01-30 21:19:03 -0800

    vanity

--- components/MyNavbar.tsx
+++ components/MyNavbar.tsx
@@ -6,12 +6,12 @@ const MyNavbar = () => {
         <nav className='bg-yellow-800'>
             <ul>
                 <li>
-                    <a href='/'>
+                    <a href='/' className='bg-yellow-600'>
                         Home
                     </a>
                 </li>
                 <li>
-                    <a href='/add'>
+                    <a href='/add' className='bg-yellow-600'>
                         Add
                     </a>
                 </li>
