e5a1edcbce628ed4e10aff6452ec51be99f6ff3b
Daniel Law <dlaw37@my.bcit.ca>
2025-02-05 11:03:48 -0800

    fixing deployment

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,22 @@
-// next.config.js
-import type { NextConfig } from "next";
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+   basePath: '/CafedeaWaitlist',
 
-const nextConfig: NextConfig = {
-  // Enable static export
-  output: "export",
-  // Prefix for the subdirectory (replace with the name of your repo or folder)
-  basePath: "/CafedeaWaitlist",
-  assetPrefix: "/CafedeaWaitlist/",
-  trailingSlash: true, // Ensures paths have trailing slashes (GitHub Pages needs this)
-};
-
-export default nextConfig;
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
+  // // Prefix for the subdirectory (replace with the name of your repo or folder)
+  // basePath: "/CafedeaWaitlist",
+  // assetPrefix: "/CafedeaWaitlist/",
+  // trailingSlash: true, // Ensures paths have trailing slashes (GitHub Pages needs this)
